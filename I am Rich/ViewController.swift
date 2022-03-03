43cb053f0555df4d5584960a4b2ea5db09d595cb
Lasal <lasalshettiarachchi458@gmail.com>
2022-03-03 20:15:37 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Lasal Hettiarachchi on 2022-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
